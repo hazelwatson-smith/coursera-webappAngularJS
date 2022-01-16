ad16a324bc6365c6dcb0089633af0bece2b6e033
hazelwatson-smith <hazel.watson-smith@soulmachines.com>
2022-01-17 12:20:14 +1300

    Assignment 1

---
+++ assignment1/assignment1-starter-code/app.js
@@ -0,0 +1,32 @@
+(function () {
+'use strict';
+
+angular.module('LunchCheck', [])
+.controller('LunchCheckController', LunchCheckController);
+
+LunchCheckController.$inject = ['$scope'];
+function LunchCheckController($scope) {
+  $scope.food = ""
+  $scope.lunchItems = 0
+
+  $scope.checkLunch = function () {
+    var foodArray = $scope.food.split(",")
+    $scope.lunchItems = foodArray.length;
+    console.log($scope.lunchItems)
+  };
+
+  $scope.lunchResult = function () {
+    if($scope.lunchItems == 0 || $scope.food == ""){return ""
+  }else{
+    if($scope.lunchItems > 0 & $scope.lunchItems <=3){
+      return "Enjoy!"
+    } else{
+      if($scope.lunchItems > 3){
+        return "Too much!"
+      }
+    }
+  }
+  };
+}
+
+})();
